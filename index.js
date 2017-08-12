73307aa2763680ac21f858538286f7fb29608d35
johnkaick <johnkaick@gmail.com>
2017-08-11 22:50:17 -0300

    serviços e produtos

---
+++ index.js
@@ -0,0 +1,34 @@
+const hapi = require('hapi')
+
+const server = new hapi.Server()
+
+server.connection({ port: 8080 })
+
+server.register(
+    [
+        { register: require('inert') }
+    ], function (err) {
+
+        if (err) console.log(err)
+
+        server.route({
+            method: 'GET',
+            path: '/{param*}',
+            handler: {
+                directory: {
+                    path: 'public/',
+                    redirectToSlash: true,
+                    index: true
+                }
+            }
+        })
+
+    }
+)
+
+server.start(function (err) {
+    if (err) {
+        throw err
+    }
+    console.log(`Server running at: ${server.info.uri}`)
+})
